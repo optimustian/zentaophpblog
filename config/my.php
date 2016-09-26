8b6f16ab8131107c9934fc370232a887795c6212
roomanl <roomanl@live.cn>
2016-09-26 09:39:11 +0800

    Update my.php

--- config/my.php
+++ config/my.php
@@ -10,11 +10,6 @@ $config->db->port     = '3306';
 $config->db->name     = 'zentaophp'; 
 $config->db->user     = 'root'; 
 $config->db->password = '123456';
-/*$config->db->host     = '575ed73d50cff.gz.cdb.myqcloud.com';
-$config->db->port     = '10551';
-$config->db->name     = 'sddman'; 
-$config->db->user     = 'cdb_outerroot'; 
-$config->db->password = 'sql_109227175';*/
 
 /* To use master and slave database feature, uncomment this.
 $config->slaveDB->host     = 'localhost';
